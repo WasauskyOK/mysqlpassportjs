f031a23141d01e37504e5581e74008cd6d9503f7
Wasauskyok <daniechoque159@gmail.com>
2020-01-11 06:14:42 -0500

    add  .env

--- server/conection/conex.js
+++ server/conection/conex.js
@@ -1,7 +1,13 @@
+require('dotenv').config();
 import  mysql from 'mysql';
-import  cadena from '../config/config';
+//import  cadena from '../config/config';
 
-const  pool=mysql.createPool(cadena.database);
+const  pool=mysql.createPool({
+    host:process.env.host,
+    user:process.env.user,
+    password:process.env.password,
+    database:process.env.database
+});
 pool.getConnection((err,conection)=>{
     if(err)throw err;
     else{
--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,9 @@
-export default {
-    database:{
-        host:"bec1rv2kxcm0re78ujeh-mysql.services.clever-cloud.com",
-        user:"ujs4ywqzyemhqhwd",
-        password:"2dvyOOUxDzC5FS7RUzHm",
-        database:"bec1rv2kxcm0re78ujeh"
+// export default {
+//     database:{
+//         host:"bec1rv2kxcm0re78ujeh-mysql.services.clever-cloud.com",
+//         user:"ujs4ywqzyemhqhwd",
+//         password:"2dvyOOUxDzC5FS7RUzHm",
+//         database:"bec1rv2kxcm0re78ujeh"
 
-    }
-}
+//     }
+// }
